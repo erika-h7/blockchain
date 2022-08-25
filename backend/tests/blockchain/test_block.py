6ce6c0568303ab2a40e8af26c71576d9a7b3522b
erika-h7 <erika.herrera1@outlook.com>
2022-08-25 12:51:47 -0700

    Creating proof of work hash

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,4 @@
-from backend.blockchain.block import Block
+from backend.blockchain.block import Block, GENESIS_DATA
 
 def test_mine_block():
     last_block = Block.genesis()
@@ -13,3 +13,11 @@ def test_genesis():
     genesis = Block.genesis()
 
     assert isinstance(genesis, Block)
+    
+    # assert genesis.timestamp == GENESIS_DATA['timestamp']
+    # assert genesis.last_hash == GENESIS_DATA['last_hash']
+    # assert genesis.hash == GENESIS_DATA['hash']
+    # assert genesis.data == GENESIS_DATA['data']
+
+    for key, value in GENESIS_DATA.items():
+        getattr(genesis, key) == value
